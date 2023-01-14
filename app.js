c604b22f6c7c6ef7173ae4169aebd1ab1c7b45ee
Никита <kolesnikov.n666@gmail.com>
2023-01-14 18:26:15 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+import {initializeApp} from "firebase/app"
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from "firebase/storage"
+import {upload} from './upload'
+import {firebaseConfig} from "./firebaseConfig";
+
+const app = initializeApp(firebaseConfig)
+const storage = getStorage(app)
+
+upload('#file', {
+    multi: true,
+    accept:['.png','.jpg','.jpeg','.gif'],
+    onUpload(files, blocks){
+        files.forEach((file, index) => {
+          const storageRef = ref(storage, `images/${file.name}`)
+          const uploadTask = uploadBytesResumable(storageRef, file);
+          uploadTask.on('state_changed', 
+          (snapshot) => {            
+            const progress =((snapshot.bytesTransferred / snapshot.totalBytes) * 100).toFixed(0) + '%'
+            const blocksSelector = blocks[index].querySelector('.preview-info-progress')
+             blocksSelector.textContent = progress
+             blocksSelector.style.width = progress
+            console.log('Upload is ' + progress + '% done')
+            switch (snapshot.state) {
+              case 'paused':
+                console.log('Upload is paused')
+                break;
+              case 'running':
+                console.log('Upload is running')
+                break;
+            }
+          }, 
+          (error) => {
+            console.log(error)
+          }, 
+          () => {           
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+
+              console.log('Файл доступен по ссылке ', downloadURL)
+            });
+          }
+        );
+        })
+    }
+})
+
